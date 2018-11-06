34f066df3c9964b8b86e91b3f19e7a09bb00f7d8
Student <example@example.com>
2018-11-05 16:47:40 -0800

    Initialize, with multiple components.

---
+++ src/components/Ticket.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Ticket(props){
+  return (
+    <div>
+      <style jsx>{`
+         div {
+           background-color: red;
+         }
+         h3 {
+           font-family: fantasy;
+         }
+       `}</style>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+Ticket.propTypes = {
+  names: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  issue: PropTypes.string
+};
+
+export default Ticket;
